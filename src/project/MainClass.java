0c90aa85e1b727e2c9c870e4db67839701d3efbd
AlanKangD <kno12350@gmail.com>
2021-08-26 16:18:43 +0900

    수정

--- src/project/MainClass.java
+++ src/project/MainClass.java
@@ -1,5 +1,27 @@
 package project;
 
+import java.util.Scanner;
+
 public class MainClass {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num;
+		while(true) {
+			System.out.println("1.강동원 2.박수민 3.김나현 4.김선우 5.김도윤 6.이호준");
+			num = sc.nextInt();
+			switch(num) {
+			case 1: break;
+			case 2: break;
+			case 3: break;
+			case 4: break;
+			case 5: break;
+			case 6: break;
+			
+			}
+			
+		}
+		
+		
+	}
 
 }
