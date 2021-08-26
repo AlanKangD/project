8c14143afc21a438ddf1f223274920aa6ffd9dd7
AlanKangD <kno12350@gmail.com>
2021-08-26 16:39:03 +0900

    지금 작성중

---
+++ src/alan/TestAlanFile.java
@@ -0,0 +1,8 @@
+package alan;
+
+public class TestAlanFile {
+	public void display() {
+		System.out.println("개똥 기능 완성");
+	}
+
+}
--- src/project/MainClass.java
+++ src/project/MainClass.java
@@ -2,6 +2,8 @@ package project;
 
 import java.util.Scanner;
 
+import alan.TestAlanFile;
+
 public class MainClass {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -9,8 +11,9 @@ public class MainClass {
 		while(true) {
 			System.out.println("1.강동원 2.박수민 3.김나현 4.김선우 5.김도윤 6.이호준");
 			num = sc.nextInt();
+			TestAlanFile alan = new TestAlanFile();
 			switch(num) {
-			case 1: break;
+			case 1:  alan.display(); break;
 			case 2: break;
 			case 3: break;
 			case 4: break;
