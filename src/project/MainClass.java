1a6d3b1a5bb6f808f551e6ca96edd59fd4133861
AlanKangD <kno12350@gmail.com>
2021-08-30 15:04:02 +0900

    확인

---
+++ src/project/MainClass.java
@@ -0,0 +1,33 @@
+package project;
+
+import java.util.Scanner;
+
+import alan.TestAlanFile;
+
+public class MainClass {
+	public static void main(String[] args) {
+		soomin_test_class soomin = new soomin_test_class();
+		Scanner sc = new Scanner(System.in);
+		int num;
+		while(true) {
+			System.out.println("1.강동원 2.박수민 3.김나현 4.김선우 5.김도윤 6.이호준");
+			num = sc.nextInt();
+			TestAlanFile alan = new TestAlanFile();
+			switch(num) {
+			case 1:  alan.display(); break;
+			case 2: 
+				soomin.one();			
+				break;
+			case 3: break;
+			case 4: break;
+			case 5: break;
+			case 6: break;
+			
+			}
+			
+		}
+		
+		
+	}
+
+}
---
+++ src/project/soomin_test_class.java
@@ -0,0 +1,8 @@
+package project;
+
+public class soomin_test_class {
+	public void one() {
+		System.out.println("1 + 1 은" + 3 + " 입니다.");
+	}
+
+}
